c7057f51f707b3ae3cc953f055d0ebcc6b641dfc
Christian M <chriamue@gmail.com>
2023-10-08 09:31:57 +0200

    changed description

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -12,22 +12,10 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">RebuildDAO</span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+            A DAO-driven platform where citizens and global supporters can propose, vote, and fund reconstruction
+            projects using NFTs. Each NFT represents a specific reconstruction project or asset in the city.
+          </code>
         </div>
       </div>
 
