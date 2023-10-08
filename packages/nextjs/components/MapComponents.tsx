b1f6396aa380cd677b46f85aee125b77a5c35112
Christian M <chriamue@gmail.com>
2023-10-08 07:51:48 +0200

    improved ui

--- packages/nextjs/components/MapComponents.tsx
+++ packages/nextjs/components/MapComponents.tsx
@@ -1,45 +1,55 @@
+import { LatLng } from "leaflet";
+// Re-uses images from ~leaflet package
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
-import 'leaflet-defaulticon-compatibility';
 
 type Proposal = {
-  id: number;
+  id: bigint;
   name: string;
   description: string;
   location: string;
   url: string;
-  fundingGoal: number;
-  fundsRaised: number;
+  fundingGoal: bigint;
+  fundsRaised: bigint;
 };
 
 type MapComponentProps = {
-  proposals?: Proposal[];
+  proposals?: ReadonlyArray<Proposal>;
 };
 
 const MapComponent: React.FC<MapComponentProps> = ({ proposals }) => {
-  const position = [41.890 as number, 12.49 as number]; // default latitude and longitude
+  const position = new LatLng(41.89 as number, 12.49 as number); // default latitude and longitude
 
-  const markers = proposals?.map(proposal => {
-    const [lat, lng] = proposal.location.split(",").map(Number);
-    return {
-      id: proposal.id,
-      latitude: lat,
-      longitude: lng,
-      name: proposal.name,
-      description: proposal.description,
-      url: proposal.url,
-    };
-  }) ?? [];
+  const markers =
+    proposals?.map(proposal => {
+      const [lat, lng] = proposal.location.replaceAll(" ", "").split(",").map(Number);
+      return {
+        id: proposal.id,
+        latitude: lat,
+        longitude: lng,
+        name: proposal.name,
+        description: proposal.description,
+        url: proposal.url,
+        fundingGoal: proposal.fundingGoal,
+        fundsRaised: proposal.fundsRaised,
+      };
+    }) ?? [];
 
   return (
-    <MapContainer center={position} zoom={13} style={{ width: "100%", height: "400px" }}>
+    <MapContainer center={position} zoom={13} style={{ width: "100%", height: "100%" }}>
       {markers.map(marker => (
-        <Marker key={marker.id} position={[marker.latitude, marker.longitude]}>
+        <Marker key={marker.id} position={new LatLng(marker.latitude, marker.longitude)}>
           <Popup>
             <h1>{marker.name}</h1>
             <p>{marker.description}</p>
             <img src={marker.url} alt="proposal image" />
+            <p>
+              Funding Goal: {marker.fundingGoal.toString()}
+              <br />
+              Funding Raised: {marker.fundsRaised.toString()}
+            </p>
           </Popup>
         </Marker>
       ))}
--- packages/nextjs/components/example-ui/ContractData.tsx
+++ packages/nextjs/components/example-ui/ContractData.tsx
@@ -1,46 +1,41 @@
 import { useState } from "react";
-import {
-  useAnimationConfig,
-  useScaffoldContractRead,
-} from "~~/hooks/scaffold-eth";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { HareIcon } from "./assets/HareIcon";
+import { useAnimationConfig, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
 const DynamicMapComponent = dynamic(
-  () => import('../MapComponents'),
-  { ssr: false } // This will load the component only on the client side
+  () => import("../MapComponents"),
+  { ssr: false }, // This will load the component only on the client side
 );
 
 export const ContractData = () => {
   const [transitionEnabled, setTransitionEnabled] = useState(true);
 
-
   const { data: totalCounter } = useScaffoldContractRead({
     contractName: "ProjectProposal",
-    functionName: "totalSupply"
+    functionName: "totalSupply",
   });
 
   const { data: currentProposals, isLoading: isProposalsLoading } = useScaffoldContractRead({
     contractName: "ProjectProposal",
-    functionName: "getAllProposals"
+    functionName: "getAllProposals",
   });
 
   const { showAnimation } = useAnimationConfig(totalCounter);
 
   console.log(currentProposals);
 
-  if(isProposalsLoading) {
-    return (<></>)
+  if (isProposalsLoading) {
+    return <></>;
   }
+
   return (
     <div className="flex flex-col justify-center items-center bg-base-300 py-10 px-5 sm:px-0 lg:py-auto max-w-[100vw]">
       <HareIcon className="absolute right-0 bottom-24" />
-      <div className="flex rounded-2xl" style={{ width: '100%', height: '800px' }}>
-        <DynamicMapComponent proposals={currentProposals} />
-      </div>
       <div
-        className={`flex flex-col max-w-md bg-base-200 bg-opacity-70 rounded-2xl shadow-lg px-5 py-2 w-full ${showAnimation ? "animate-zoom" : ""
-          }`}
+        className={`flex flex-col max-w-md bg-base-200 bg-opacity-70 rounded-2xl shadow-lg px-5 py-2 w-full ${
+          showAnimation ? "animate-zoom" : ""
+        }`}
       >
         <div className="flex justify-between w-full">
           <button
@@ -50,8 +45,9 @@ export const ContractData = () => {
             }}
           >
             <div
-              className={`absolute inset-0 bg-center bg-no-repeat bg-black transition-opacity ${transitionEnabled ? "opacity-0" : "opacity-100"
-                }`}
+              className={`absolute inset-0 bg-center bg-no-repeat bg-black transition-opacity ${
+                transitionEnabled ? "opacity-0" : "opacity-100"
+              }`}
             />
           </button>
           <div className="bg-secondary border border-primary rounded-xl flex">
@@ -61,6 +57,9 @@ export const ContractData = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex flex-col rounded-2xl py-10" style={{ width: "80%", height: "800px" }}>
+        <DynamicMapComponent proposals={currentProposals} />
       </div>
     </div>
   );
